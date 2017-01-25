5693356c17e82a27b34be071c352325ef10ed8b0
n1try <mailerdinand-muetsch.de>
2017-01-25 21:40:48 +0100

    Safe browe script use Google safe browse API.

--- snippets/safe_browsing_checker.js
+++ snippets/safe_browsing_checker.js
@@ -1,18 +1,40 @@
 'use strict';
 
-const request = require('request'),
-    url = require('url'),
+/*
+https://developers.google.com/safe-browsing/v3/lookup-guide
+https://console.developers.google.com/apis/credentials?project=anchr-156715
+https://github.com/n1try/node-safe-browse
+*/
+
+const url = require('url'),
     MongoClient = require('mongodb').MongoClient,
     fs = require('fs'),
     path = require('path'),
     winston = require('winston'),
+    SafeBrowse = require('safe-browse'),
+    api = SafeBrowse.Api('AIzaSyCjjrpLaBBvF202Gr2xzqgmX4nJAHtFML8'),
     DB_URL = 'mongodb://anchr:Agiwovibu146@ferdinand-muetsch.de:27017/anchr',
-    COLLECTION = 'shortlinks',
-    THRESHOLD = 1;
+    COLLECTION = 'shortlinks';
+
+Array.prototype.getUnique = function() {
+    var u = {},
+        a = [];
+    for (var i = 0, l = this.length; i < l; ++i) {
+        if (u.hasOwnProperty(this[i])) {
+            continue;
+        }
+        a.push(this[i]);
+        u[this[i]] = 1;
+    }
+    return a;
+}
 
 winston.add(winston.transports.File, { filename: 'safe_browsing_checker.log' });
 
 let processedIds = [];
+let dbConnection = null;
+let total = 0;
+let numProcessed = 0;
 
 try {
     processedIds = JSON.parse(fs.readFileSync(path.normalize(__dirname + '/history.json')));
@@ -20,41 +42,53 @@ try {
 
 MongoClient.connect(DB_URL, function(err, db) {
     let coll = db.collection(COLLECTION);
+    dbConnection = db;
 
     coll.find({ _id: { $nin: processedIds } }).toArray((err, results) => {
-        if (!results.length) notifyFinished(-1, db);
-        results.forEach((result) => {
-            let host = url.parse(result.url).hostname;
-            winston.log('debug', 'Processing ' + host);
-            request('https://safeweb.norton.com/report/show?url=' + host, (err, res, body) => {
-                processedIds.push(result._id);
-                if (body && extractNumber(body) >= THRESHOLD) {
-                    coll.deleteOne({ _id: result._id }, (err, ok) => {
-                        if (err) winston.log('info', '[FAILED] ' + result.url);
-                        else winston.log('info', '[DELETED] ' + result.url);
-                        notifyFinished(results.length, db);
+        if (!results.length) exit(-1);
+        let resultMap = results.reduce(function(map, obj) {
+            if (map[obj.url]) map[obj.url].push(obj._id);
+            else map[obj.url] = [obj._id];
+            return map;
+        }, {});
+
+        api.lookup(Object.keys(resultMap).getUnique(), (err, data) => {
+            if (err) return exit(err);
+            data = data.data;
+            total = Object.keys(data).length;
+
+            for (let key in data) {
+                if (data[key] !== 'ok') {
+                    coll.deleteMany({ url: key }, (err, ok) => {
+                        if (err) winston.log('info', '[FAILED] ' + key);
+                        else {
+                            winston.log('info', '[DELETED] ' + key);
+                            numProcessed += resultMap[key].length;
+                            processedIds = processedIds.concat(resultMap[key]);
+                        }
+                        notifyFinished();
                     });
-                } else notifyFinished(results.length, db);
-            });
+                } else {
+                    numProcessed += resultMap[key].length;
+                    processedIds = processedIds.concat(resultMap[key]);
+                    notifyFinished();
+                }
+            }
         });
     });
 });
 
-function extractNumber(textString) {
-    const searchString = '<b> Total threats</b> on this site: ';
-    let sub = textString.substr(textString.indexOf(searchString));
-    sub = sub.substr(searchString.length);
-    let num = parseInt(sub.substr(0, sub.indexOf(' ')));
-    return num || -1;
-}
-
 let c = 0;
 
-function notifyFinished(total, db) {
+function notifyFinished() {
     c++;
-    if (c === total || total === -1) {
-        winston.log('info', `Processed ${total === -1 ? 0 : c} entries.`);
-        fs.writeFileSync(path.normalize(__dirname + '/history.json'), JSON.stringify(processedIds));
-        db.close();
-    }
+    if (c >= total) exit(null);
+}
+
+function exit(err) {
+    if (!err) winston.log('info', `Processed ${numProcessed} entries.`);
+    else winston.log('info', `[ERROR] ${err.message}`);
+    fs.writeFileSync(path.normalize(__dirname + '/history.json'), JSON.stringify(processedIds));
+    dbConnection.close();
+    process.exit(0);
 }
