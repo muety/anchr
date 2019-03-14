efd8b3065bbc99564eb7e19190ce73b17202b402
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2019-03-14 17:04:09 +0100

    Enable ETag.

--- config/express.js
+++ config/express.js
@@ -31,12 +31,10 @@ module.exports = function(app, config) {
     }]
   }));
 
-  app.set('etag', false);
-
   if (env === 'development') {
     app.use(function (req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,HEAD');
       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
       // intercept OPTIONS method
