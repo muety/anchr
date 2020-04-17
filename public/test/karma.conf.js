df30d6ab865a0da543e78a5c63ccbc3deda38641
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2020-04-17 22:29:56 +0200

    Override main script of crypto-js for wiredep.

--- public/test/karma.conf.js
+++ public/test/karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function(config) {
             'bower_components/snackbarjs/dist/snackbar.min.js',
             'bower_components/ng-file-upload/ng-file-upload.js',
             'bower_components/angular-jwt/dist/angular-jwt.js',
-            'bower_components/crypto-js/index.js',
+            'bower_components/crypto-js/crypto-js.js',
             'bower_components/angular-mocks/angular-mocks.js',
             // endbower
             "app/scripts/**/*.js",
