d889c44fe8ed07c97e0502b706602adbdb1e907e
Ferdinand Mütsch <ferdinand@muetsch.io>
2021-01-15 09:39:31 +0100

    chore: show loading indicator while fetching link description (resolve #36)

--- public/app/scripts/controllers/collection.js
+++ public/app/scripts/controllers/collection.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('anchrClientApp')
-    .controller('CollectionCtrl', ['$scope', '$rootScope', 'Collection', 'Remote', 'Snackbar', '$window', '$timeout', function($scope, $rootScope, Collection, Remote, Snackbar, $window, $timeout) {
+    .controller('CollectionCtrl', ['$scope', '$rootScope', 'Collection', 'Remote', 'Snackbar', '$window', '$timeout', function ($scope, $rootScope, Collection, Remote, Snackbar, $window, $timeout) {
 
         var collections = [];
         var fetchTitleDebounce = null;
 
         /* Either id or index! */
-        $scope.setActiveCollection = function(id, cached) {
+        $scope.setActiveCollection = function (id, cached) {
             if ($scope.data.active === id && cached) return;
 
             function setActive() {
@@ -17,10 +17,10 @@ angular.module('anchrClientApp')
             }
 
             if (!cached) {
-                Collection.collection.get({ _id: id }, function(result) {
+                Collection.collection.get({ _id: id }, function (result) {
                     collections[findCollection(collections, id)] = result;
                     setActive();
-                }, function(err) {
+                }, function (err) {
                     Snackbar.show("Failed to fetch collection " + getCollection(collections, id).name + " from server: " + err.data.error);
                 });
             } else {
@@ -28,20 +28,20 @@ angular.module('anchrClientApp')
             }
         };
 
-        $scope.getCollection = function(id) {
+        $scope.getCollection = function (id) {
             for (var i = 0; i < collections.length; i++) {
                 if (collections[i]._id == id) return collections[i];
             }
             return null;
         };
 
-        $scope.saveLink = function(collId, link, desc) {
+        $scope.saveLink = function (collId, link, desc) {
             var l = new Collection.links({
                 collId: collId,
                 url: link,
                 description: desc
             });
-            l.$save(function(result) {
+            l.$save(function (result) {
                 $scope.data.linkInput = '';
                 $scope.data.descriptionInput = '';
                 $scope.getCollection(collId).links.push({
@@ -50,89 +50,99 @@ angular.module('anchrClientApp')
                     description: result.description,
                     date: result.date
                 });
-            }, function(err) {
+            }, function (err) {
                 Snackbar.show('Failed to save link: ' + err.data.error);
             });
         };
 
-        $scope.deleteLink = function(collId, linkId) {
+        $scope.deleteLink = function (collId, linkId) {
             var l = new Collection.links({
                 collId: collId,
                 _id: linkId
             });
-            l.$delete(function(result) {
+            l.$delete(function (result) {
                 var c = $scope.getCollection(collId);
                 c.links.splice(findLinkInCollection(c, linkId), 1);
-            }, function(err) {
+            }, function (err) {
                 Snackbar.show('Failed to delete link: ' + err.data.error);
             });
         };
 
-        $scope.deleteCollection = function(collId) {
+        $scope.deleteCollection = function (collId) {
             var c = $scope.getCollection(collId);
-            c.$delete(function(result) {
+            c.$delete(function (result) {
                 collections.splice(findCollection(collections, collId), 1);
                 $scope.setActiveCollection(collections[0]._id || 0);
-            }, function(err) {
+            }, function (err) {
                 Snackbar.show('Failed to delete collection: ' + err.data.error);
             });
         }
 
-        $scope.addCollection = function(name) {
+        $scope.addCollection = function (name) {
             new Collection.collection({
                 name: name,
                 links: []
-            }).$save(function(result) {
+            }).$save(function (result) {
                 collections.push(result);
                 $scope.toggleNewCollection();
                 $scope.setActiveCollection(result._id, true);
-            }, function(err) {
+            }, function (err) {
                 Snackbar.show('Failed to create new collection: ' + err.data.error);
             });
         };
 
-        $scope.shareCollection = function(collId) {
-            Collection.collection.update({_id: collId, shared: true}, function(result) {
+        $scope.shareCollection = function (collId) {
+            Collection.collection.update({ _id: collId, shared: true }, function (result) {
                 getCollection(collections, collId).shared = true;
                 Snackbar.show('Collection shared.');
-            }, function(err) {
+            }, function (err) {
                 Snackbar.show('Failed to share collection: ' + err.data.error);
             })
         };
 
-        $scope.renameCollection = function(collId) {
-            Collection.collection.update({_id: collId, name: $scope.data.editing.newName}, function(result) {
+        $scope.renameCollection = function (collId) {
+            Collection.collection.update({ _id: collId, name: $scope.data.editing.newName }, function (result) {
                 getCollection(collections, collId).name = $scope.data.editing.newName;
                 $scope.data.editing = null;
                 Snackbar.show('Collection updated.');
                 $('#modalRename').modal('toggle');
-            }, function(err) {
+            }, function (err) {
                 Snackbar.show('Failed to update collection: ' + err.data.error);
             })
         };
 
-        $scope.onLinkChanged = function() {
+        $scope.onLinkChanged = function () {
             if (!$scope.data.linkInput ||
-                !$scope.data.linkInput.length > 1 ||
-                !$scope.data.linkInput.length > 1 ||
-                ($scope.data.descriptionInput && $scope.data.descriptionInput.length)) return;
-            if (fetchTitleDebounce != null) $timeout.cancel(fetchTitleDebounce);
-            fetchTitleDebounce = $timeout(function() {
-                Remote.page.get({ url: $scope.data.linkInput }, function(result) {
+                $scope.data.linkInput.length <= 0 ||
+                ($scope.data.descriptionInput && $scope.data.descriptionInput.length)) {
+                return;
+            }
+
+            if (fetchTitleDebounce != null) {
+                $timeout.cancel(fetchTitleDebounce);
+            }
+
+            fetchTitleDebounce = $timeout(function () {
+                $scope.data.loading = true;
+                Remote.page.get({ url: $scope.data.linkInput }, function (result) {
+                    $scope.data.loading = false;
                     if (!result.title) return;
                     $scope.data.descriptionInput = result.title;
-                }, function(err) {});
+                }, function (err) {
+                    $scope.data.loading = false;
+                });
             }, 500);
         };
 
-        $scope.clear = function() {
+        $scope.clear = function () {
             $scope.data = {
                 collections: collections,
                 active: 0,
-                search: ''
+                search: '',
+                loading: false
             };
 
-            Collection.collection.query(function(result) {
+            Collection.collection.query(function (result) {
                 collections = result;
                 $scope.data.collections = collections;
 
@@ -142,9 +152,9 @@ angular.module('anchrClientApp')
             });
         };
 
-        $scope.toggleNewCollection = function() {
+        $scope.toggleNewCollection = function () {
             $scope.adding = !$scope.adding;
-            $timeout(function() {
+            $timeout(function () {
                 if ($scope.adding) $window.onControllerEvent('newCollection');
             });
         };
