ddbc990934eec34233dbd7e78e4cd27e6b00fd2f
Ferdinand Mütsch <ferdinand@muetsch.io>
2024-10-16 10:34:47 +0200

    fix: mongodb init script (resolve #70)

--- scripts/mongo-init.sh
+++ scripts/mongo-init.sh
@@ -1,3 +1,1 @@
-mongosh -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --eval "
-use \"$MONGO_INITDB_DATABASE\";
-db.createUser({user: \"$DB_USER\", pwd: \"$DB_PASSWORD\", roles: [{ role: \"dbOwner\", db: \"$MONGO_INITDB_DATABASE\" }]});"
+mongosh -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --eval "db = db.getSiblingDB('$MONGO_INITDB_DATABASE'); db.createUser({user: '$DB_USER', pwd: '$DB_PASSWORD', roles: [{ role: 'dbOwner', db: '$MONGO_INITDB_DATABASE' }]});"
